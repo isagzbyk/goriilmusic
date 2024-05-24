b50dfdeab89ae72f02b40362a484d97df7f72642
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-24 16:49:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "180"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/9db6ae360e0a7b833abff.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/goril-04-21.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/goril-04-21.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dertkonagi")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Uh-skC23MONkMTU0")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "BRANDED KIGN")  
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "✨𝗚𝗼𝗿𝗶𝗹 𝗠𝘂𝘇𝗶𝗸✨")  
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/goril-04-21.jpg"
